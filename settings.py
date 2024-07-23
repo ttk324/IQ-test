dec996ca85c084a7381e61b3b32df9b91c93593e
Tiffany TSE <60918278+ttk324@users.noreply.github.com>
2024-07-23 11:53:35 +0900

    settings.py

--- settings.py
+++ settings.py
@@ -2,27 +2,9 @@ from os import environ
 SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
 SESSION_CONFIGS = [
 
-    dict(name='Tiffany_IPD2022_firstorderbig', num_demo_participants=None,
-         app_sequence=['Tiffany_IPD2022_firstorderbig', ], participation_fee=500,
-         ),
-    dict(name='Tiffany_IPD2022_baselinebig', num_demo_participants=None,
-         app_sequence=['Tiffany_IPD2022_baselinebig', ], participation_fee=500,
-         ),
-    dict(name='Tiffany_IPD2022_first_big_full', num_demo_participants=None,
-         app_sequence=['Tiffany_IPD2022_first_big_full', ], participation_fee=500,
-         ),
-    dict(name='Tiffany_IPD2022_afterexp', num_demo_participants=None,
-         app_sequence=['Tiffany_Raven_Kamei', 'Tiffany_MPL_risk_Kamei', 'Tiffany_survey_IPD2022', ],
-         ),
 
     dict(name='Tiffany_Raven_Kamei', num_demo_participants=None,
          app_sequence=['Tiffany_Raven_Kamei',  ],
-         ),
-    dict(name='Tiffany_MPL_risk_Kamei', num_demo_participants=None,
-         app_sequence=['Tiffany_MPL_risk_Kamei',],
-         ),
-    dict(name='Tiffany_survey_IPD2022', num_demo_participants=None,
-         app_sequence=['Tiffany_survey_IPD2022', ],
          ),
 
 ]
